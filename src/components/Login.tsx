d06d4cb77d1df1e923cd5890884a36b9a84b1202
ADRIANH <rosalesadrian017@gmail.com>
2026-03-23 22:17:51 -0600

    Login's Use State variables created also HandleInputChangeMethod was created

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,20 +1,34 @@
+import React, { useState } from 'react';
 import '../form.css'
 const Login = ()=>{
+    //Use state of Username declared
+        var [formData,setFormData]= useState({
+            username:"",
+            password:""
+        });
+
+        const HandleInputChange=(e: React.ChangeEvent<HTMLInputElement>)=>{
+            const {name , value}= e.target;
+            setFormData({
+                ...formData,[name]:value
+            });
+        };
 return(
     <div id='login'>
         <form action="" method="post">
         <ul>
             <li>
                 <label htmlFor="user_name">UserName:</label>
-                <input type="text" name="user_name" id="user_name" placeholder="Username" />
+                <input type="text" name="username" id="user_name" placeholder="Username" value={formData.username} onChange={HandleInputChange}/>
             </li>
             <li>
                 <label htmlFor="user_name">Password:</label>
-                <input type="text" name="pass" id="pass" placeholder="Password"/>
+                <input type="text" name="password" id="pass" placeholder="Password" value={formData.password} onChange={HandleInputChange}/>
             </li>
         </ul>
         <button type="submit">Log In</button>
         </form>
+
     </div>
 
 );
